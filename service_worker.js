1a3230ce7f0c1f960d2b4a45f04d38e9f5accea3
ddewald-elec <152433308+ddewald-elec@users.noreply.github.com>
2026-03-16 14:15:08 +0200

    Refactor service worker code formatting

--- service_worker.js
+++ service_worker.js
@@ -6,27 +6,17 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
-
 event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
+caches.open(CACHE_NAME).then(cache => {
 return cache.addAll(urlsToCache);
 })
-
 );
-
 });
 
 self.addEventListener("fetch", event => {
-
 event.respondWith(
-
-caches.match(event.request)
-.then(response => {
+caches.match(event.request).then(response => {
 return response || fetch(event.request);
 })
-
 );
-
 });
