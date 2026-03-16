382309ab559b1bc120f95cec828285ca00a89cbf
ddewald-elec <152433308+ddewald-elec@users.noreply.github.com>
2026-03-16 14:37:28 +0200

    Enhance service worker registration with error handling
    Added error handling for service worker registration.

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,10 @@ if ("serviceWorker" in navigator) {
 
 navigator.serviceWorker.register("./service-worker.js")
 .then(function() {
-
 console.log("Service Worker Registered");
-
+})
+.catch(function(error) {
+console.log("Service Worker failed:", error);
 });
 
 }
